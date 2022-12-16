ea4d9df3dc121c2afc46f78e479d509f88d6299b
Drei4 <antipin-lukinf@mail.ru>
2022-12-16 12:32:56 +0300

    add hw1

--- HOME_WORK/hw1/L1.java
+++ HOME_WORK/hw1/L1.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-// На основе кода с урока добавить еще один класс продукта, 
-// (Молоко, шоколад ,и т.п., можно добавить несколько классов),
-//  они должны наследоваться от класса Product, надо переопределить метод toString и 
-// попробовать включить эти классы в VendingMachine, вывести список товаров из VendingMachine
 
 public class L1 {
     public static void main(String[] args) {
